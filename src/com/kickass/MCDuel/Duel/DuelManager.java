7eef399a66881f669e961f1463da8bde0e84d215
Daniel Breton <danielcraig94@hotmail.co.uk>
2014-07-31 22:49:20 +0100

    Added timer to boundary damage thang

--- src/com/kickass/MCDuel/Duel/DuelManager.java
+++ src/com/kickass/MCDuel/Duel/DuelManager.java
@@ -1,51 +1,56 @@
 package com.kickass.MCDuel.Duel;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.UUID;
 
 import org.bukkit.entity.Player;
 
 public class DuelManager {
-	
+
 	public static void addDuel(Duel duel) {
-		for(Player player : duel.getPlayers()) {
+		for (Player player : duel.getPlayers()) {
 			duels.put(player.getUniqueId(), duel);
 		}
 	}
-	
+
 	public static void removeDuel(Player player) {
 		removeDuel(player.getUniqueId());
 	}
-	
+
 	public static void removeDuel(UUID uuid) {
 		Duel duel = duels.get(uuid);
-		if(uuid != null) {
+		if (uuid != null) {
 			removeDuel(duel);
 		}
 	}
-	
+
 	public static void removeDuel(Duel duel) {
-		for(Player player : duel.getPlayers()) {
+		for (Player player : duel.getPlayers()) {
 			duels.remove(player.getUniqueId());
 		}
 	}
-	
+
 	public static boolean isPlayerDueling(Player p) {
 		return isPlayerDueling(p.getUniqueId());
 	}
-	
+
 	public static boolean isPlayerDueling(UUID uuid) {
 		return duels.containsKey(uuid);
 	}
-	
+
 	public static Duel getDuel(Player player) {
 		return getDuel(player.getUniqueId());
 	}
-	
+
 	public static Duel getDuel(UUID uuid) {
 		return duels.get(uuid);
 	}
-	
+
+	public static Collection<Duel> getDuels() {
+		return duels.values();
+	}
+
 	private static final HashMap<UUID, Duel> duels = new HashMap<UUID, Duel>();
 
 }
--- src/com/kickass/MCDuel/MCDuel.java
+++ src/com/kickass/MCDuel/MCDuel.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.kickass.MCDuel.Arena.ArenaManager;
 import com.kickass.MCDuel.Commands.CommandHandler;
+import com.kickass.MCDuel.Duel.OutOfBoundsHandler;
 import com.kickass.MCDuel.Listeners.ListenerManager;
 
 public class MCDuel extends JavaPlugin {
@@ -12,16 +13,17 @@ public class MCDuel extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		instance = this;
-		
+
 		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new ArenaManager(), 0, 6);
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new OutOfBoundsHandler(), 0, 10);
 		ListenerManager.initialize(this);
 		getCommand("duel").setExecutor(new CommandHandler());
 	}
-	
+
 	public static MCDuel getInstance() {
 		return instance;
 	}
-	
+
 	private static MCDuel instance;
 
 }
